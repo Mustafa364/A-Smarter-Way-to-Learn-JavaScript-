d4b08f911e566f877db45cc886af4862730dae26
Ghulam Mustafa <116076196+Mustafa364@users.noreply.github.com>
2022-11-09 01:31:20 +0500

    Add files via upload

---
+++ javascript Assingment#03/app.js
@@ -0,0 +1,57 @@
+
+// var fruit1 = "Apple";
+// var fruit2 = "banana";
+// var fruit3 = "Orange";
+// var fruit4 = "Grapse";
+// var fruit5 = "Mango";
+
+
+// var a =["Mango", "Grapse", "Orange", "banana", "Apple"] 
+
+// a[1] = "XYZ";
+// a[0] = "Apple"
+// a[12] = "pomagranate"
+
+// console.log (a);
+
+// a.push("pomagranate"); last mn ek value add karna
+// a.pop(); last sy ek value hatana
+
+// a.shift(); start sy ek cut karega
+// a.unshift("pomagranate") start sy ek add karega
+
+// var b = a.slice(0, 2);
+// console.log (b);
+
+// a.splice(1, 3); 
+// var b = a.splice(2, 1, "ABC", "XYZ", "MOC");
+// console.log (b);
+
+// console.log (a);
+
+
+// var a = []
+
+// for(var i=0; i<10; i++){
+// a.push("abc"+ i)
+// }
+// console.log(a);
+
+
+// var a = ["abc", "mk", "lkj", "iop", "xyz"]
+
+// for(var i=0; i< a.length; i++){
+//     console.log(a[i]);
+// }
+
+
+// var a ;
+
+// for(a = 1; i<= 10; i++){
+//     alert(a[i]);
+// }
+
+// var cities = ["karachi", "lahore", "Hyderabad"];
+// for (var i = 0; i < cities.length; i++){
+//     console.log(cities[i])
+//}
